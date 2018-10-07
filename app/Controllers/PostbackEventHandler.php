ded5e49a4582f065cc1541d36218282ab3ca96bf
薄皮 <wu30944@gmail.com>
2018-10-07 23:51:29 +0800

    test

--- app/Controllers/PostbackEventHandler.php
+++ app/Controllers/PostbackEventHandler.php
@@ -17,27 +17,28 @@ class PostbackEventHandler extends EventHandler
     public function handle()
     {
         Log::info("PostbackEvent");
+        error_log('1');
         $jsonObj = json_decode($this->req->getBody());
           //回答的使用者
-        error_log($jsonObj);
-        if(property_exists($this->jsonObj->{"events"}[0]->{"source"}->{"userId"})){
-            $userId=$jsonObj->{"events"}[0]->{"source"}->{"userId"};
-        }
-
-
-        if(property_exists($jsonObj->{"events"}[0]->{"source"},'groupId')){
-          $groupId=$jsonObj->{"events"}[0]->{"source"}->{"groupId"};
-      }
-
-      if(property_exists($jsonObj->{"events"}[0]->{"source"},'roomId')){
-          $roomId=$jsonObj->{"events"}[0]->{"source"}->{"roomId"};
-      }
-
-      $data=$jsonObj->{"events"}[0]->{"postback"}->{"data"};
-        error_log($data);
-//      $dataObj = explode('|',$data);
-//      Log::info("User:".$userId." Answer:".$dataObj[1]." question_id:".$dataObj[0]);
-      return  $this->replyText($data);
+        error_log('2');
+//        if(property_exists($this->jsonObj->{"events"}[0]->{"source"}->{"userId"})){
+//            $userId=$jsonObj->{"events"}[0]->{"source"}->{"userId"};
+//        }
+//
+//
+//        if(property_exists($jsonObj->{"events"}[0]->{"source"},'groupId')){
+//          $groupId=$jsonObj->{"events"}[0]->{"source"}->{"groupId"};
+//      }
+//
+//      if(property_exists($jsonObj->{"events"}[0]->{"source"},'roomId')){
+//          $roomId=$jsonObj->{"events"}[0]->{"source"}->{"roomId"};
+//      }
+//
+//      $data=$jsonObj->{"events"}[0]->{"postback"}->{"data"};
+//        error_log($data);
+////      $dataObj = explode('|',$data);
+////      Log::info("User:".$userId." Answer:".$dataObj[1]." question_id:".$dataObj[0]);
+      return  $this->replyText(transk('default.TalkOtherThing'));
 
   }
 }
